cf3e62586e4032945df6d5ffcc6f306be67f64fd
Desilvey20 <101611124+Desilvey20@users.noreply.github.com>
2022-04-25 11:24:10 -0700

    table

--- scripts/display.js
+++ scripts/display.js
@@ -32,13 +32,16 @@ function displayCards(){
     for(let i=0;i<petsalon.pets.length;i++){
         let pet = petsalon.pets[i];
         tmp+=`
-        <div class="pet">
-        <h4>${pet.name}</h4>
-        <label>Age:${pet.age}</label>
-        <label>Gender:${pet.gender}</label>
-        <label>Breed:${pet.breed}</label>
-        <label>Owner:${pet.owner}</label>
-        <label>Contact Owner:${pet.contact}</label>
+        <div id="${pet.id}" class="pet">
+           <div class="pet-header">
+               <h4>${pet.name}</h4>
+               <button onclick="deletePet(${pet.id});"> 🗑️ </button>
+           </div>
+           <label>Age:${pet.age}</label>
+           <label>Gender:${pet.gender}</label>
+           <label>Breed:${pet.breed}</label>
+           <label>Owner:${pet.owner}</label>
+           <label>Contact Owner:${pet.contact}</label>
         </div>
         `;
     
@@ -48,6 +51,26 @@ function displayCards(){
 
 //display the pets in table
 
-
+function displayTable(){
+    let tr="";
+    for(let i=0;i<petsalon.pets.length;i++){
+        let pet = petsalon.pets[i];
+        tr+=`
+        
+        
+        <tr id="${pet.id}">
+            <td>${pet.name}</td>
+            <td>${pet.age}</td>
+            <td>${pet.gender}</td>
+            <td>${pet.breed}</td>
+            <td>${pet.service}</td>
+            <td>${pet.owner}</td>
+            <td>${pet.contact}</td>
+            <td><button onclick="deletePet();">🗑️</button></td>
+        </tr>
+        </div>`;
+    }
+    document.getElementById("pets").innerHTML=tr;
+}
 
 
--- scripts/register.js
+++ scripts/register.js
@@ -45,6 +45,8 @@ let petsalon = {
 
 console.log(petsalon);
 
+let c=0
+
 function pet(name,age,gender,breed,service,ownername,contact){
     this.name=name;
     this.age=age;
@@ -53,6 +55,7 @@ function pet(name,age,gender,breed,service,ownername,contact){
     this.service=service;
     this.owner=ownername;
     this.contact=contact;
+    this.id=c++;//increase the var 1 everytime that is used
 
 }
 //get the info from the inputs and store the information
@@ -90,7 +93,7 @@ console.log(thePet)
     //push the pet into the array
     petsalon.pets.push(thePet);
     //clear the inputs
-    displayCards();
+    displayTable();
     clearinputs();
 }
 
@@ -104,8 +107,57 @@ function clearinputs(){
     inputContact.value="";
 }
 
+function deletePet(petId){
+    console.log("Deleting" + petId);
+    let deleteIndex;
+    //previous actions
+       //we need an id in the pet constructor
+       //we need a delete button in the html
+    // in this function
+       // travel the array (for loop)
+       for(let i=0;i<petsalon.pets.length;i++){
+           let pet = petsalon.pets[i];
+           if(petId==pet.id){
+               deleteIndex=i;
+               console.log("I found it in position:" + i)
+
+           }
+
+       }
+       // find the id (if)
+       // get the position in the array(store in a var)
+       // remove from the array (splice())
+       petsalon.pets.splice(deleteIndex,1);
+       // remove from the html (remove())
+       document.getElementById(petId).remove();
+       //display to the user a message
+    //
+
+}
+
+function searchPet(){
+    let searchString = document.getElementById("txtSearch").value;
+    console.log("Searching " + searchString);
+
+for(let i=0;i<petsalon.pets.length;i++){
+    let pet = petsalon.pets[i];
+    if(searchString.toLowerCase() === pet.name.toLowerCase()){
+        //alert("I found it");
+        document.getElementById(pet.id).classList.add("highlight");
+    }else{
+        document.getElementById(pet.id).classList.remove("highlight");
+    }
+}
+    // in this function
+       //travel the array (for loop)
+       //find the id (if)
+       //highlight the result
+}
+
+
  let Scooby = new pet("Scooby",2,"boxer","male","grooming","Bella","760-555-555");
 let Enrique = new pet("Enrique",3,"mixed","male","grooming","David","555-555-5555");
+let Lady = new pet("Lady",11,"pomaranian","female","nails cut","Jessica","985-555-5555")
 
-petsalon.pets.push(Scooby,Enrique);
-displayCards();
+petsalon.pets.push(Scooby,Enrique,Lady);
+displayTable();
